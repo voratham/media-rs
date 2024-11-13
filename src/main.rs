7ad26520a1f381cd31f6a244140670bb96a7c2bc
voratham.s <voratham.s@linecorp.com>
2024-11-13 15:34:09 +0700

    chore: pattern matching with enum

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
+use std::default;
+
 #[derive(Debug)]
 enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     Audiobook { title: String },
+    Audiobook2 { title: String },
 }
 
 impl Media {
     fn description(&self) -> String {
-        if let Media::Book { title, author } = self {
-            format!("book: {} {} ", title, author)
-        } else if let Media::Movie { title, director } = self {
-            format!("movie: {} {} ", title, director)
-        } else if let Media::Audiobook { title } = self {
-            format!("audio book: {}", title)
-        } else {
-            String::from("Media description")
+        // if let Media::Book { title, author } = self {
+        //     format!("book: {} {} ", title, author)
+        // } else if let Media::Movie { title, director } = self {
+        //     format!("movie: {} {} ", title, director)
+        // } else if let Media::Audiobook { title } = self {
+        //     format!("audio book: {}", title)
+        // } else {
+        //     String::from("Media description")
+        // }
+
+        match self {
+            Media::Book { title, author } => {
+                format!("book: {} {} ", title, author)
+            }
+            Media::Movie { title, director } => {
+                format!("movie: {} {} ", title, director)
+            }
+            Media::Audiobook { title } => {
+                format!("audio book: {}", title)
+            }
+            _ => String::from("Media description"),
         }
     }
 }
@@ -42,7 +58,13 @@ fn main() {
     println!("{}", good_movie.description());
     println!("{}", bad_book.description());
 
-    print_media(auidobook);
-    print_media(good_movie);
-    print_media(bad_book);
+    let auidobook2 = Media::Audiobook2 {
+        title: String::from("An Audiobook"),
+    };
+    println!("{}", auidobook2.description());
+
+
+    // print_media(auidobook);
+    // print_media(good_movie);
+    // print_media(bad_book);
 }
